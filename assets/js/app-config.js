ea9d90c32e7972e3cbb18970bed5b4d5cb74e3ec
Vijeet Rathore <vijeetrathore@gmail.com>
2026-08-28 00:36:49 +0530

    url

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -6,5 +6,5 @@
    device's Settings page.
    ============================================ */
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxdTk2etrCtOhNMCCFN4lCgniL0v9508KhGwEsV3m5UyWOO3gj5fg2xKfRa6h7bsCg7SQ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyYXbKz6_ER0m_LXPjB5SrgtRExkTbD9APkU30ZcEfFPaEUq3mrMEUtAhlyBMIkiGLE2w/exec";
 const GAS_TOKEN = "getgorgeous_2026";
